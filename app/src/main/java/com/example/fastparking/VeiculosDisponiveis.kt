0389b70e2e9af9c673197805a684284304d680ea
mariana <marianabicla@gmail.com>
2022-05-31 16:53:18 -0300

    inicio de telas

---
+++ app/src/main/java/com/example/fastparking/VeiculosDisponiveis.kt
@@ -0,0 +1,11 @@
+package com.example.fastparking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VeiculosDisponiveis : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_veiculos_disponiveis)
+    }
+}
